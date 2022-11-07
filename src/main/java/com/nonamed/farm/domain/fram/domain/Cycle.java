f84649f12c64d3b499da83a687ed79e1065a2738
hee <cmlgml094356@gmail.com>
2022-11-07 19:26:33 +0900

    create : cycle entity

---
+++ src/main/java/com/nonamed/farm/domain/fram/domain/Cycle.java
@@ -0,0 +1,41 @@
+package com.nonamed.farm.domain.fram.domain;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Cycle {
+
+	@Id
+	private LocalDate date;
+
+	private Boolean isWater; // id에 해당하는 날짜에 물을 주었는지
+	private Boolean isLight;
+
+	@Column(nullable = false)
+	private String farmId;
+
+	@Builder
+	private Cycle(String farmId, Boolean isWater, Boolean isLight) {
+		this.date = LocalDate.now();
+		this.isWater = isWater;
+		this.isLight = isLight;
+		this.farmId = farmId;
+	}
+
+	public void isState(Boolean isWater, Boolean isLight) {
+		this.isWater = isWater;
+		this.isLight = isLight;
+	}
+
+}
---
+++ src/main/java/com/nonamed/farm/domain/fram/domain/repository/CycleRepository.java
@@ -0,0 +1,8 @@
+package com.nonamed.farm.domain.fram.domain.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nonamed.farm.domain.fram.domain.Cycle;
+
+public interface CycleRepository extends CrudRepository<Cycle, Long> {
+}
