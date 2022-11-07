48a57680937f63c826e4e8d084d610f893505611
hee <cmlgml094356@gmail.com>
2022-11-07 19:27:21 +0900

    create : base entity

---
+++ src/main/java/com/nonamed/farm/global/entity/BaseIdEntity.java
@@ -0,0 +1,22 @@
+package com.nonamed.farm.global.entity;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseIdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ src/main/java/com/nonamed/farm/global/entity/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.nonamed.farm.global.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity extends BaseIdEntity {
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDate createdDate;
+
+}
