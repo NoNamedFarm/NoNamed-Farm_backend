fe3b6f49b0e146740e4dba0e630b2f2abdd1479d
hee <cmlgml094356@gmail.com>
2022-11-29 13:53:53 +0900

    update : 헨들러 수정

--- src/main/java/com/nonamed/farm/global/exception/handler/NonamedExceptionHandler.java
+++ src/main/java/com/nonamed/farm/global/exception/handler/NonamedExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.nonamed.farm.global.exception.handler;
 
-import javax.validation.ValidationException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.nonamed.farm.global.exception.NonamedException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class NonamedExceptionHandler {
 
@@ -19,12 +24,34 @@ public class NonamedExceptionHandler {
 
 	@ExceptionHandler(NullPointerException.class)
 	protected ResponseEntity<ErrorResponse> HandleNullPointerException(final NullPointerException e) {
+		log.error("[NullPointerException] : " + e.getMessage());
 		return new ResponseEntity<>(new ErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
 	}
 
-	@ExceptionHandler(ValidationException.class)
-	protected ResponseEntity<ErrorResponse> HandleValidationException(final ValidationException e) {
-		return new ResponseEntity<>(new ErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ErrorResponse> HandleValidationException(final MethodArgumentNotValidException e) {
+		BindingResult bindingResult = e.getBindingResult();
+
+		StringBuilder builder = new StringBuilder();
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			builder.append(fieldError.getField()).append("(은)는 ");
+			builder.append(fieldError.getDefaultMessage()).append("   ");
+		}
+
+		log.error("[HandleValidationException] : " + e.getMessage());
+		return new ResponseEntity<>(new ErrorResponse(400, builder.toString()), HttpStatus.BAD_REQUEST);
 	}
 
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+		log.error("[handleHttpRequestMethodNotSupportedException] : " + e.getMessage());
+		return new ResponseEntity<>(new ErrorResponse(405, e.getMessage()), HttpStatus.METHOD_NOT_ALLOWED);
+	}
+
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+		log.error("[exception] : " + e.getMessage());
+		return new ResponseEntity<>(new ErrorResponse(500, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 }
